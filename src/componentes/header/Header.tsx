488e532eec15a768a503906e2ab583f14616eddd
FelpinDaBlaze <felipe.sarro@estudante.ifms.edu.br>
2023-08-24 08:56:06 -0400

    sim

---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,29 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        
+        <img src='barbiezinha.webp' alt="Logo"></img>
+        
+        <nav className="navbar">
+          <ul>
+            
+            <li>
+              <a href="#">Notícia</a>
+            </li>
+            <li>
+              <a href="#">Sobre</a>
+            </li>
+            <li>
+              <a href="#">Contato</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      
+    
+    </div>
+  );
+}
