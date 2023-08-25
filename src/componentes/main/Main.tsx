d88226d17696ac9cf9c9508754a9030bef920c47
FelpinDaBlaze <felipe.sarro@estudante.ifms.edu.br>
2023-08-25 13:03:15 -0400

    atu3

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -3,7 +3,21 @@ import './Main.css';
 export default function Main() {
   return (
     <div>
-      
+
+      <div>
+
+        <h1 className='h1-1'>
+          Olá pessoas,
+        </h1>
+
+        <h2 className='h2-1'>Eu sou Estudante do</h2>
+
+        <h2 className='h2-2'>IFMS Campus de</h2>
+
+        <h2 className='h2-3'>Navirai</h2>
+
+      </div>
+
     </div>
   );
 }
