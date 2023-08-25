bb5165b450fe52fa784e163355bb300e709e6be7
FelpinDaBlaze <felipe.sarro@estudante.ifms.edu.br>
2023-08-25 08:56:45 -0400

    atu3

--- src/componentes/footer/Footer.tsx
+++ src/componentes/footer/Footer.tsx
@@ -27,15 +27,14 @@ export default function Footer() {
                 <ul className='ul1'>
                   <li><a href="#">Email</a></li>
                   <li><a href="#">Telefone</a></li>
-                  <li><a href="#">Chat</a></li>
                 </ul>
             </div>
 
             <div>
                 <ul className='ul2'>
-                  <li ><a href="#"><img src="zapzap.png" alt="WhatsApp" /></a></li>
-                  <li><a href="#"><img src="https://www.freepnglogos.com/uploads/logo-ig-png/logo-ig-lighting-and-furniture-design-studio-aqua-creations-32.png" alt="Instagram" /></a></li>
-                  <li><a href="#"><img src="https://www.pngplay.com/wp-content/uploads/9/Facebook-No-Background.png" alt="Facebook" /></a></li>
+                  <li><a href="#"><img src="Instagram.png"/></a></li>
+                  <li><a href="#"><img src="Linkedin.png"/></a></li>
+                  <li><a href="#"><img src="Git.png"/></a></li>
                 </ul>
             </div>
           </div>
